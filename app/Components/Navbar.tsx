dd9e0922d44d8760b53f54505a327bbe02080c25
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-09-15 09:56:23 +0200

    My first tweet card

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../Icon/Icons"
 
 export const Navbar = () => {
     return(
-        <section className="flex justify-between items-center h-20">
+        <section className="flex fixed z-20 justify-between items-center h-20 bg-black w-full">
             <div className="absolute w-full top-0 left-0 z-10 h-20">
             </div>
             <div className="z-10 flex gap-3 items-center font-bold text-2xl">
--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,20 @@ export default function Home() {
       <section className="h-screen relative">
         <div
           className="
-        absolute right-0 z-10 bg-white p-2 rounded-md flex gap-3 items-center
+        absolute top-28 md:right-0 z-20 bg-white p-2 rounded-md flex gap-3 items-center
         "
         >
           <span className="h-4 w-4 p-2 bg-sky-400 rounded-full absolute -right-1 -top-1 animate-ping "></span>
           <span className="h-1 w-1 p-2 bg-sky-400 rounded-full absolute -right-1 -top-1"></span>
-          <Image
-            className=" h-12 w-12 rounded-full object-cover "
-            src={"/images/camilo.JPG"}
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
+          <div className=" w-32 md:w-14">
+            <Image
+              className=" w-14 h-14 md:h-14 md:w-14 rounded-full object-cover "
+              src={"/images/camilo.JPG"}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
           <div className="flex flex-col">
             <h1 className="font-bold text-sm text-gray-800 flex gap-1 items-center">
               {"@CamiloCarreno"}
@@ -33,7 +35,7 @@ export default function Home() {
                 {"• 18:03"}
               </span>
             </h1>
-            <p className="text-black text-xs w-96">
+            <p className="text-black text-xs w-full md:w-96">
               {
                 "Just realized coding is like baking a cake: a little syntax, a pinch of logic, and a lot of 'trial and error' frosting! 🍰💻 "
               }
@@ -44,7 +46,7 @@ export default function Home() {
           </div>
         </div>
         <Background />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-3/4 text-center w-96 md:w-full">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/3 text-center w-96 md:w-full">
           <h1 className="text-white flex flex-col gap-2 md:gap-5 text-7xl font-bold md:text-8xl md:flex-row md:justify-center mb-6">
             <span>Camilo</span>
             <GradientText text="Carreño." colors={["#FF6101", "#FD7F2B"]} />

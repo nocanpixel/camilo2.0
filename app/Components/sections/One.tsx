bf299d9db580186bd058b14ac4a3624087fb2da6
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-10-11 14:13:41 +0200

    Changing titles

--- app/Components/sections/One.tsx
+++ app/Components/sections/One.tsx
@@ -4,10 +4,9 @@ import { projects } from "@/app/utils/projects";
 import { Project } from "@/app/types/interfaces";
 
 const One = () => {
-
   const numGroups = 3;
   const groups: Project[][] = Array.from({ length: numGroups }, () => []);
-  
+
   projects.forEach((project, index) => {
     const groupIndex = index % numGroups;
     groups[groupIndex].push(project);
@@ -16,7 +15,7 @@ const One = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold text-center pt-8">
-        <span
+        {/* <span
           style={{
             WebkitBackgroundClip: "text",
             WebkitTextFillColor: "transparent",
@@ -25,10 +24,20 @@ const One = () => {
           }}
         >
           Projects
-        </span>
+        </span> */}
       </h1>
       <h2 className="flex flex-col md:flex-row gap-4 items-center justify-center pt-4">
-        <span className="text-3xl text-white">My recent work</span>
+        <span
+          className="text-4xl font-bold "
+          style={{
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            backgroundImage:
+              "linear-gradient(90deg, oklch(59.59% 0.24 255.09156059071347), oklch(81.58% 0.189 190.74037768509325))",
+          }}
+        >
+          My recent work
+        </span>
         <span className="text-lg text-center md:text-start text-gray-400">
           {"Here are a few design projects I've worked on."}
         </span>
--- app/Components/sections/Skills.tsx
+++ app/Components/sections/Skills.tsx
@@ -6,12 +6,17 @@ import { Popover } from "../Popover";
 export const Skills = () => {
   return (
     <div className="py-5">
-      <h1 className="text-4xl font-bold text-center">
+      <h1 className="flex flex-col md:flex-row gap-4 items-center justify-center pt-4">
         <GradientText
-          text="Skills"
+          className="text-4xl font-bold"
+          text="Technical Skills"
           colors={["oklch(49.07% 0.272 300.45)", "oklch(64.53% 0.292 2.47)"]}
         />
+        <span className="text-lg text-center md:text-start text-gray-400">
+          {"snapshot of my technical skills."}
+        </span>
       </h1>
+
       <div className="grid grid-cols-2 grid-rows-2 md:grid-cols-5 md:grid-rows-6 gap-4 pt-10">
         {skills?.map((skill) => {
           // const truncatedName =

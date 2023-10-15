5836819d349b44c172aadb7a873391eb661a914e
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-10-15 21:14:22 +0200

    Google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Camilo portfolio",
-  description: "Experienced developer with a passion for solving complex problems through elegant code. Proficient in multiple languages, frameworks, and tools. Committed to delivering quality and innovative solutions for business success.",
+  description:
+    "Experienced developer with a passion for solving complex problems through elegant code. Proficient in multiple languages, frameworks, and tools. Committed to delivering quality and innovative solutions for business success.",
 };
 
 export default function RootLayout({
@@ -17,6 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5326726221343916"
+        crossOrigin="anonymous"
+      ></script>
       <body suppressHydrationWarning className={inter.className}>
         <Navbar />
         <div className="px-6 lg:px-40" id="margins">

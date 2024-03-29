2f0259960aa32ad918e6d64f002ec3b6c62a2b46
Juan Camilo Carreno Beleno <nocan@Juans-MacBook-Air.local>
2024-03-29 15:57:39 +0100

    Video fix

--- app/Components/BgVideo.tsx
+++ app/Components/BgVideo.tsx
@@ -1,23 +1,23 @@
 export function BgVideo() {
+  
     return (
-        <video
-            autoPlay
-            muted
-            loop
-            style={{
-                position: 'absolute',
-                top: '-70%',
-                left: 0,
-                width: '100%', // Set width to 100%
-                height: '100vh', // Set height to 100% viewport height
-                objectFit: 'cover', // Optional: Maintain aspect ratio if needed
-                filter: 'blur(20px)',
-                scale: 2,
-                zIndex: -1,
-              }}
-        >
-            <source src="/vector.mp4" type="video/mp4" />
-            {/* Add fallback for browsers that don't support video */}
-        </video>
-    )
-}
+      <video
+        autoPlay
+        muted
+        style={{
+          position: 'absolute',
+          top: '-50%',
+          left: 0,
+          width: '100%',
+          height: '100vh',
+          objectFit: 'cover',
+          filter:'blur(20px)',
+          scale: 1.5,
+          zIndex: -1,
+        }}
+      >
+        <source src="/vector.mp4" type="video/mp4" />
+        {/* Add fallback for browsers that don't support video */}
+      </video>
+    );
+  }

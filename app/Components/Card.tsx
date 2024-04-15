dcc64ec3430086cd64f11214881b7f5371255108
Juan Camilo Carreno Beleno <nocan@Juans-MacBook-Air.local>
2024-04-15 15:13:08 +0200

    Much better

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -13,11 +13,21 @@ const Card = (props: Props) => {
 
   return (
     <div className="relative">
-      <Link className={`absolute z-10 -right-2 -top-2 bg-black rounded-full ${props.githubUrl===undefined&&'cursor-not-allowed pointer-events-none '}`} href={props.githubUrl?props.githubUrl:'#'} target="_blank">
-      <div className="w-6 transition ease-in delay-100 hover:scale-125">
+      {props.githubUrl !== undefined ? (
+        <Link href={props.githubUrl} passHref>
+          <div className="absolute z-10 -right-2 -top-2 bg-black rounded-full">
+            <div className="w-6 transition ease-in delay-100 hover:scale-125">
+              <GithubIcon url={props.githubUrl} />
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <div className="absolute z-10 -right-2 -top-2 bg-black rounded-full cursor-not-allowed">
+          <div className="w-6">
             <GithubIcon url={props.githubUrl} />
           </div>
-      </Link>
+        </div>
+      )}
       <Link href={props.url} target="_blank" className={`border relative border-gray-800 cursor-pointer p-6 rounded-xl hover:border-gray-500 hover:bg-zinc-900 hover:bg-opacity-60 flex flex-col gap-3 bg-zinc-600 justify-between backdrop-filter bg-opacity-10 backdrop-blur-sm`} >
         <div className="flex justify-between">
           <h2 className="text-xl text-white">{props.title}</h2>

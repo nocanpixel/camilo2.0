68cd01701e2d91e63567483b024757d69c4f8aeb
Juan Camilo Carreno Beleno <nocan@Juans-MacBook-Air.local>
2024-04-15 04:46:01 +0200

    Job experiences

--- app/utils/experience.ts
+++ app/utils/experience.ts
@@ -1,6 +1,20 @@
 export const experience = [
     {
         id:1,
+        company: "ReDI School of Digital Integration Malmö",
+        en: {
+            date: "April 2023 - August 2023 · 5 months",
+            rol: "",
+            description: " Teaching a web development to a class of 20 students in my second language, English."
+        },
+        es: {
+            date: "",
+            rol: "",
+            description: "Enseñando desarrollo web a una clase de 20 estudiantes en mi segundo idioma, inglés."
+        }
+    },
+    {
+        id:2,
         company: "Venturit",
         en: {
             date: "June 2022 - July 2023 · 1 year 1 month",
@@ -11,6 +25,34 @@ export const experience = [
             date: "Junio 2022 - Julio 2023 · 1 año 1 mes",
             rol: "Desarrollador de Software",
             description: "Trabajé con varios frameworks y bibliotecas para desarrollar componentes reutilizables y modularizar el código, colaborando estrechamente con el equipo de backend."
+        }
+    },
+    {
+        id:3,
+        company: "Ecoyuma",
+        en: {
+            date: "2020 - 2022 · 2 years",
+            rol: "Full Stack engineer",
+            description: "Collaborating in a team to build a cutting-edge inventory management system tailored specifically for the agricultural sector."
+        },
+        es: {
+            date: "2020 - 2022 · 2 años",
+            rol: "Ingeniero Full Stack",
+            description: "Colaboré en un equipo para construir un sistema de gestión de inventario de vanguardia diseñado específicamente para el sector agrícola"
+        }
+    },
+    {
+        id:4,
+        company: "Freelance - Self employed",
+        en: {
+            date: "2019 - 2021 · 2 years",
+            rol: "Full Stack developer",
+            description: "Worked with Figma to create wireframes, prototypes and UI design. Collaboratin with stakeholders to gather requirements and refine system features."
+        },
+        es: {
+            date: "2019 - 2021 · 2 años",
+            rol: "Desarrollador Full Stack",
+            description: "Trabajé con Figma para crear esquemas, prototipos y diseño de interfaz de usuario. Colaboré con las partes interesadas para recopilar requisitos y refinar las características del sistema."
         }
     }
 ]

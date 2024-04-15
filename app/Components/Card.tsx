a13570681f928d4b93562daf34e93761252dc62c
System Administrator <root@Juans-MacBook-Air.local>
2024-04-15 14:03:00 +0200

    at least works on desktop

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -13,11 +13,17 @@ const Card = (props: Props) => {
 
   return (
     <div className="relative">
-        <Link className={`absolute z-10 -right-2 -top-2 bg-black rounded-full ${props.githubUrl==="#"&&' pointer-events-none cursor-not-allowed'}`} href={props.githubUrl} target="_blank">
+      {props.githubUrl ? (
+        <Link className="absolute z-10 -right-2 -top-2 bg-black rounded-full " href={props.githubUrl} target="_blank">
           <div className="w-6 transition ease-in delay-100 hover:scale-125 flex justify-center items-center">
-            <GithubIcon color={props.githubUrl} />
+            <GithubIcon url={props.githubUrl}/>
           </div>
         </Link>
+      ) : (
+        <div className="w-6 cursor-not-allowed absolute z-10 -right-2 -top-2 bg-black rounded-full">
+          <GithubIcon color="#2d313b" />
+        </div>
+      )}
       <Link href={props.url} target="_blank" className={`border relative border-gray-800 cursor-pointer p-6 rounded-xl hover:border-gray-500 hover:bg-zinc-900 hover:bg-opacity-60 flex flex-col gap-3 bg-zinc-600 justify-between backdrop-filter bg-opacity-10 backdrop-blur-sm`} >
         <div className="flex justify-between">
           <h2 className="text-xl text-white">{props.title}</h2>
